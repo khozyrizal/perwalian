c40c520ae0a978a7d8cef725eefdcf48c656eb96
khozyrizal <mitrania69@yahoo.co.id>
2014-05-13 20:06:38 +0700

    Create MahasiswaInterface.java

---
+++ MahasiswaInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adi.thinkoop.perwalianonline.interfaces;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ *
+ * @author adi nuralim
+ */
+public interface MahasiswaInterface {
+
+    Map getAll() throws SQLException;
+}
